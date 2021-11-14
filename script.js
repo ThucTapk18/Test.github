9f597dd8ba0558a97746af8e0d49c7970912ff9b
Your Name <benanh25@gmail.com>
2021-11-14 14:41:40 +0700

    Update web

--- script.js
+++ script.js
@@ -8,19 +8,35 @@ function makeid() {
     var number = '0123456789';
     var numberLength = number.length;
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ )
- {
-
-     result  += characters.charAt(Math.floor(Math.random() * charactersLength)); 
-     result2  += number.charAt(Math.floor(Math.random() * numberLength)); 
-	 
-    }  
-	
+    if(length>=1 && length<=50){
+        for ( var i = 0; i < length; i++ )
+        {
     
-    document.getElementById("randomString").innerHTML = result; 
-
-    document.getElementById("randomNumber").innerHTML = result2;
+         result  += characters.charAt(Math.floor(Math.random() * charactersLength)); 
+         result2  += number.charAt(Math.floor(Math.random() * numberLength)); 
+         
+        }  
+        
+        
+        document.getElementById("randomString").innerHTML = result; 
+    
+        document.getElementById("randomNumber").innerHTML = result2;
+    }
+    else{
+        alert("Bạn chỉ được nhập số từ 1 đến 50")
+    }
 	
 	 
 }
+function copyToClipboard(elementId) {
 
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+  
+    document.execCommand("copy");
+
+    document.body.removeChild(aux);
+  
+  }
